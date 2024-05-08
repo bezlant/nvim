1cc02862527d446c7f27af14f664c1d0a9d0a9eb
abezlyudniy <abezlyudniy@ozon.ru>
2024-05-08 13:29:55 +0800

    feat: large files

--- init.lua
+++ init.lua
@@ -1,3 +1,17 @@
+-- huge is over 100KB
+local is_huge_file = function(bufnr)
+	bufnr = bufnr or 0
+	local max_filesize = 100 * 1024 -- 100 KB
+	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
+	if ok and stats and stats.size > max_filesize then
+		return true
+	end
+end
+
+if is_huge_file() then
+	return
+end
+
 require('config.options')
 require('config.lazy')
 require('config.keymaps')
