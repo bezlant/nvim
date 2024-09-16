28fbeecdd46faf0509b7c28a96ee2e84b3c972fd
abezlyudniy <abezlyudniy@ozon.ru>
2024-09-16 17:00:09 +0300

    feat: eslint

--- lua/lsp/functions.lua
+++ lua/lsp/functions.lua
@@ -5,9 +5,10 @@ function M.enable_format_on_save()
   vim.api.nvim_create_autocmd("BufWritePre", {
     callback = function()
       local root_dir = vim.fn.getcwd()
-      local eslintrc_json = root_dir .. "/.eslintrc.json"
-      local eslintrc_js = root_dir .. "/.eslintrc.js"
+      local eslint_config_mjs = root_dir .. "/eslint.config.mjs"
       local eslintrc = root_dir .. "/.eslintrc"
+      local eslintrc_js = root_dir .. "/.eslintrc.js"
+      local eslintrc_json = root_dir .. "/.eslintrc.json"
 
       -- Check if eslint LSP is active
       local active_clients = vim.lsp.buf_get_clients()
@@ -20,7 +21,7 @@ function M.enable_format_on_save()
         end
       end
 
-      local found_eslint_config = vim.fn.filereadable(eslintrc) == 1 or vim.fn.filereadable(eslintrc_json) == 1 or vim.fn.filereadable(eslintrc_js) == 1
+      local found_eslint_config = vim.fn.filereadable(eslintrc) == 1 or vim.fn.filereadable(eslintrc_json) == 1 or vim.fn.filereadable(eslintrc_js) == 1 or vim.fn.filereadable(eslint_config_mjs) == 1
 
       if eslint_is_active and found_eslint_config then
         vim.cmd("EslintFixAll")
