b85bf9970b60a959d2d0c29198081838cc037d30
Mikhail Kuznetsov <mikhail.n.kuznetsov@gmail.com>
2024-10-08 22:42:52 +0700

    ref: remove old handler

--- init.lua
+++ init.lua
@@ -1,20 +1,6 @@
 if vim.loader then vim.loader.enable() end
 -- ^^ enables new modern loader that byte-compiles lua
 
--- huge is over 100KB
-local is_huge_file = function(bufnr)
-	bufnr = bufnr or 0
-	local max_filesize = 100 * 1024 -- 100 KB
-	local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(bufnr))
-	if ok and stats and stats.size > max_filesize then
-		return true
-	end
-end
-
-if is_huge_file() then
-	return
-end
-
 require("config.options")
 require("config.lazy")
 require("config.keymaps")
