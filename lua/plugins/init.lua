9bb98d385d5462e9fe6c3529633b0fb88fde06eb
abezlyudniy <abezlyudniy@ozon.ru>
2024-10-24 16:54:13 +0300

    feat: hardtime

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -34,4 +34,13 @@ return {
     "wakatime/vim-wakatime",
     lazy = false,
   },
+  {
+    "m4xshen/hardtime.nvim",
+    dependencies = { "MunifTanjim/nui.nvim", "nvim-lua/plenary.nvim" },
+    config = function()
+      require("hardtime").setup({
+        disable_mouse = false,
+      })
+    end,
+  },
 }
