d7044a3d94f2b6115b725ca0f40eb70374bdf103
abezlyudniy <abezlyudniy@ozon.ru>
2025-04-22 15:35:06 +0700

    feat: lazy

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -1,43 +1,8 @@
--- Return to the previous location in the file on open
-vim.api.nvim_create_autocmd(
-  "BufReadPost",
-  { command = [[if line("'\"") > 1 && line("'\"") <= line("$") | exe "normal! g'\"" | endif]] }
-)
-
--- Disable diagnostics in node_modules (0 is current buffer only)
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = "*/node_modules/*", command = "lua vim.diagnostic.disable(0)" }
-)
-
--- Autoread buffer on an external change
-vim.api.nvim_create_autocmd({ "FocusGained", "BufEnter" }, { command = "checktime" })
-
--- Show `` in specific files
-vim.api.nvim_create_autocmd(
-  { "BufRead", "BufNewFile" },
-  { pattern = { "*.txt", "*.md", "*.json" }, command = "setlocal conceallevel=0" }
-)
-
--- Easily close useless buffers
-vim.api.nvim_create_autocmd("FileType", {
-  pattern = { "sqls_output", "help", "startuptime", "qf", "lspinfo", "httpRequest" },
-  command = [[ nnoremap <buffer><silent> q :close<CR> ]],
-})
-
-vim.api.nvim_create_autocmd("FileType", { pattern = { "man" }, command = [[ nnoremap <buffer><silent> q :quit<CR> ]] })
-
--- Fix that god damn formatoptions (for some reason have to do it on every BufEnter)
--- Prevent annoying comment continuation when inserting a new line with 'O'
-vim.api.nvim_create_autocmd("BufEnter", {
-  callback = function()
-    vim.opt.formatoptions:remove({ "c", "r", "o" })
-  end,
-})
-
--- autoupdate plugins
-vim.api.nvim_create_autocmd("VimEnter", {
-  callback = function()
-    require("lazy").update({ show = false })
-  end,
-})
+-- Autocmds are automatically loaded on the VeryLazy event
+-- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
+--
+-- Add any additional autocmds here
+-- with `vim.api.nvim_create_autocmd`
+--
+-- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
+-- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
