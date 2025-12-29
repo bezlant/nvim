f9c3b344c56ac90c3a31185437b69527aa6fe710
bezlant <abezlyudniy@Snowcrash.local>
2025-12-29 22:30:40 +0700

    feat: treesitter main branch

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,19 +1,32 @@
 ---@type LazySpec
 return {
+  -- Core treesitter (main branch - provides queries only)
   {
     "nvim-treesitter/nvim-treesitter",
-    event = "BufReadPre",
-    dependencies = {
-      "JoosepAlviste/nvim-ts-context-commentstring",
-      "nvim-treesitter/nvim-treesitter-context",
-      "nvim-treesitter/nvim-treesitter-textobjects",
-      "RRethy/nvim-treesitter-textsubjects",
-    },
-
+    branch = "main",
+    lazy = false,
+    -- Disable nvim-treesitter's built-in module system (required for ts-install.nvim)
+    init = function()
+      vim.g.loaded_nvim_treesitter = 1
+    end,
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require("nvim-treesitter.configs").setup({
-        ensure_installed = {
+      -- Enable highlighting for all filetypes with a parser
+      vim.api.nvim_create_autocmd("FileType", {
+        callback = function()
+          pcall(vim.treesitter.start)
+        end,
+      })
+    end,
+  },
+
+  -- Parser installation and management
+  {
+    "lewis6991/ts-install.nvim",
+    lazy = false,
+    config = function()
+      require("ts-install").setup({
+        auto_install = true,
+        ensure_install = {
           "bash",
           "css",
           "gitcommit",
@@ -28,87 +41,143 @@ return {
           "tsx",
           "typescript",
           "vim",
+          "vimdoc",
           "vue",
           "yaml",
-          "nginx",
-        },
-        auto_install = true,
-        sync_install = true,
-
-        highlight = {
-          enable = true,
-        },
-
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            node_incremental = "v",
-            node_decremental = "V",
-          },
-        },
-
-        textobjects = {
-          move = {
-            enable = true,
-            set_jumps = true,
-            goto_next_start = {
-              ["]]"] = "@function.outer",
-              ["]m"] = "@class.outer",
-            },
-            goto_next_end = {
-              ["]["] = "@function.outer",
-              ["]M"] = "@class.outer",
-            },
-            goto_previous_start = {
-              ["[["] = "@function.outer",
-              ["[m"] = "@class.outer",
-            },
-            goto_previous_end = {
-              ["[]"] = "@function.outer",
-              ["[M"] = "@class.outer",
-            },
-          },
-
-          select = {
-            enable = true,
-            lookahead = true,
-            keymaps = {
-              ["af"] = "@function.outer",
-              ["if"] = "@function.inner",
-              ["ac"] = "@class.outer",
-              ["ic"] = "@class.inner",
-            },
-          },
-
-          swap = {
-            enable = true,
-            swap_next = {
-              ["`"] = "@parameter.inner",
-            },
-          },
-        },
-
-        textsubjects = {
-          enable = true,
-          keymaps = {
-            ["<cr>"] = "textsubjects-smart",
-          },
         },
       })
     end,
   },
 
+  -- Treesitter-aware textobjects (main branch)
+  {
+    "nvim-treesitter/nvim-treesitter-textobjects",
+    branch = "main",
+    event = "VeryLazy",
+    config = function()
+      require("nvim-treesitter-textobjects").setup({
+        select = {
+          lookahead = true,
+          include_surrounding_whitespace = false,
+        },
+        move = {
+          set_jumps = true,
+        },
+      })
+
+      -- Textobject keymaps
+      local ts_select = require("nvim-treesitter-textobjects.select")
+      local ts_move = require("nvim-treesitter-textobjects.move")
+      local ts_swap = require("nvim-treesitter-textobjects.swap")
+
+      -- Select: function
+      vim.keymap.set({ "x", "o" }, "af", function()
+        ts_select.select_textobject("@function.outer", "textobjects")
+      end, { desc = "Select outer function" })
+
+      vim.keymap.set({ "x", "o" }, "if", function()
+        ts_select.select_textobject("@function.inner", "textobjects")
+      end, { desc = "Select inner function" })
+
+      -- Select: class
+      vim.keymap.set({ "x", "o" }, "ac", function()
+        ts_select.select_textobject("@class.outer", "textobjects")
+      end, { desc = "Select outer class" })
+
+      vim.keymap.set({ "x", "o" }, "ic", function()
+        ts_select.select_textobject("@class.inner", "textobjects")
+      end, { desc = "Select inner class" })
+
+      -- Select: parameter/argument
+      vim.keymap.set({ "x", "o" }, "aa", function()
+        ts_select.select_textobject("@parameter.outer", "textobjects")
+      end, { desc = "Select outer parameter" })
+
+      vim.keymap.set({ "x", "o" }, "ia", function()
+        ts_select.select_textobject("@parameter.inner", "textobjects")
+      end, { desc = "Select inner parameter" })
+
+      -- Move
+      vim.keymap.set({ "n", "x", "o" }, "]]", function()
+        ts_move.goto_next_start("@function.outer", "textobjects")
+      end, { desc = "Next function start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "][", function()
+        ts_move.goto_next_end("@function.outer", "textobjects")
+      end, { desc = "Next function end" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[[", function()
+        ts_move.goto_previous_start("@function.outer", "textobjects")
+      end, { desc = "Previous function start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[]", function()
+        ts_move.goto_previous_end("@function.outer", "textobjects")
+      end, { desc = "Previous function end" })
+
+      vim.keymap.set({ "n", "x", "o" }, "]m", function()
+        ts_move.goto_next_start("@class.outer", "textobjects")
+      end, { desc = "Next class start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "]M", function()
+        ts_move.goto_next_end("@class.outer", "textobjects")
+      end, { desc = "Next class end" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[m", function()
+        ts_move.goto_previous_start("@class.outer", "textobjects")
+      end, { desc = "Previous class start" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[M", function()
+        ts_move.goto_previous_end("@class.outer", "textobjects")
+      end, { desc = "Previous class end" })
+
+      -- Move: parameter/argument
+      vim.keymap.set({ "n", "x", "o" }, "]a", function()
+        ts_move.goto_next_start("@parameter.outer", "textobjects")
+      end, { desc = "Next parameter" })
+
+      vim.keymap.set({ "n", "x", "o" }, "[a", function()
+        ts_move.goto_previous_start("@parameter.outer", "textobjects")
+      end, { desc = "Previous parameter" })
+
+      -- Swap parameters (Option+l / Option+h on Mac)
+      vim.keymap.set("n", "<M-l>", function()
+        ts_swap.swap_next("@parameter.inner", "textobjects")
+      end, { desc = "Swap next parameter" })
+
+      vim.keymap.set("n", "<M-h>", function()
+        ts_swap.swap_previous("@parameter.inner", "textobjects")
+      end, { desc = "Swap previous parameter" })
+    end,
+  },
+
+  -- Treesitter context (shows function/class context at top)
+  {
+    "nvim-treesitter/nvim-treesitter-context",
+    event = "VeryLazy",
+    opts = {
+      max_lines = 3,
+    },
+  },
+
+  -- Context-aware commentstring
+  {
+    "JoosepAlviste/nvim-ts-context-commentstring",
+    lazy = true,
+    opts = {
+      enable_autocmd = false,
+    },
+  },
+
+  -- Auto close/rename HTML tags
   {
     "windwp/nvim-ts-autotag",
     event = "BufReadPre",
-    config = function()
-      require("nvim-ts-autotag").setup({
-        opts = {
-          enable_close = true, -- Auto close tags
-          enable_rename = true, -- Auto rename pairs of tags
-          enable_close_on_slash = false, -- Auto close on trailing </
-        },
-      })
-    end,
+    opts = {
+      opts = {
+        enable_close = true,
+        enable_rename = true,
+        enable_close_on_slash = false,
+      },
+    },
   },
 }
