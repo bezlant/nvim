13471e412ae607f1b8349dca204ac8b74b615d12
bezlant <abezlyudniy@Snowcrash.local>
2025-12-31 11:56:57 +0800

    feat: removed unused / defaults

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -34,18 +34,6 @@ return {
             live = true,
           },
         },
-        win = {
-          input = {
-            keys = {
-              ["<C-j>"] = { "list_down", mode = { "i", "n" } },
-              ["<C-k>"] = { "list_up", mode = { "i", "n" } },
-              ["<C-d>"] = { "list_scroll_down", mode = { "i", "n" } },
-              ["<C-u>"] = { "list_scroll_up", mode = { "i", "n" } },
-              ["<C-q>"] = { "qflist", mode = { "i", "n" } },
-              ["<C-t>"] = { "trouble_open", mode = { "i", "n" } },
-            },
-          },
-        },
       },
 
       notifier = {
@@ -80,32 +68,109 @@ return {
     },
     keys = {
       -- Lazygit
-      { "<leader>g", function() Snacks.lazygit() end, desc = "LazyGit" },
+      {
+        "<leader>g",
+        function()
+          Snacks.lazygit()
+        end,
+        desc = "LazyGit",
+      },
 
       -- Picker: Files
-      { "<C-f>", function() Snacks.picker.smart() end, desc = "Smart Find" },
-      { "<leader>f", function() Snacks.picker.files() end, desc = "Find Files" },
-      { "<leader>ff", function() Snacks.picker.files({ hidden = true, ignored = true }) end, desc = "Find Files (all)" },
-      { "<leader>fr", function() Snacks.picker.resume() end, desc = "Resume Picker" },
+      {
+        "<C-f>",
+        function()
+          Snacks.picker.smart()
+        end,
+        desc = "Smart Find",
+      },
+      {
+        "<leader>f",
+        function()
+          Snacks.picker.files()
+        end,
+        desc = "Find Files",
+      },
+      {
+        "<leader>ff",
+        function()
+          Snacks.picker.files({ hidden = true, ignored = true })
+        end,
+        desc = "Find Files (all)",
+      },
+      {
+        "<leader>fr",
+        function()
+          Snacks.picker.resume()
+        end,
+        desc = "Resume Picker",
+      },
 
       -- Picker: Grep
-      { "<leader>fg", function() Snacks.picker.grep() end, desc = "Grep" },
+      {
+        "<leader>fg",
+        function()
+          Snacks.picker.grep()
+        end,
+        desc = "Grep",
+      },
 
       -- Picker: Git
-      { "<leader>fs", function() Snacks.picker.git_status() end, desc = "Git Status" },
-      { "<leader>fc", function() Snacks.picker.git_log() end, desc = "Git Commits" },
+      {
+        "<leader>fs",
+        function()
+          Snacks.picker.git_status()
+        end,
+        desc = "Git Status",
+      },
+      {
+        "<leader>fc",
+        function()
+          Snacks.picker.git_log()
+        end,
+        desc = "Git Commits",
+      },
 
       -- Picker: Buffers & Registers
-      { "<leader>fb", function() Snacks.picker.buffers() end, desc = "Buffers" },
-      { "<leader>fy", function() Snacks.picker.registers() end, desc = "Registers" },
+      {
+        "<leader>fb",
+        function()
+          Snacks.picker.buffers()
+        end,
+        desc = "Buffers",
+      },
+      {
+        "<leader>fy",
+        function()
+          Snacks.picker.registers()
+        end,
+        desc = "Registers",
+      },
 
       -- Picker: Help & Exploration
-      { "<leader>fh", function() Snacks.picker.help() end, desc = "Help" },
-      { "<leader>fk", function() Snacks.picker.keymaps() end, desc = "Keymaps" },
-      { "<leader>fl", function() require("utils.pickers").lazy_plugins() end, desc = "Lazy Plugins" },
+      {
+        "<leader>fh",
+        function()
+          Snacks.picker.help()
+        end,
+        desc = "Help",
+      },
 
-      -- Picker: Diagnostics
-      { "<leader>tw", function() Snacks.picker.diagnostics() end, desc = "Diagnostics" },
+      {
+        "<leader>fk",
+        function()
+          Snacks.picker.keymaps()
+        end,
+        desc = "Keymaps",
+      },
+
+      {
+        "<leader>fl",
+        function()
+          require("utils.pickers").lazy_plugins()
+        end,
+        desc = "Lazy Plugins",
+      },
     },
   },
 }
