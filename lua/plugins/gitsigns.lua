63a66b3375e68d0c8b79eb2e2628f5830da10cb1
bezlant <abezlyudniy@Snowcrash.local>
2025-12-31 17:55:08 +0800

    feat(gitsigns): simplified

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -2,36 +2,8 @@
 return {
   "lewis6991/gitsigns.nvim",
   event = "BufRead",
-  dependencies = { "nvim-lua/plenary.nvim" },
   opts = {
-    signcolumn = true, -- Toggle with `:Gitsigns toggle_signs`
-    numhl = false, -- Toggle with `:Gitsigns toggle_numhl`
-    linehl = false, -- Toggle with `:Gitsigns toggle_linehl`
-    word_diff = false, -- Toggle with `:Gitsigns toggle_word_diff`
-    watch_gitdir = {
-      interval = 700,
-      follow_files = true,
-    },
     attach_to_untracked = true,
-    current_line_blame = false, -- Toggle with `:Gitsigns toggle_current_line_blame`
-    current_line_blame_opts = {
-      virt_text = true,
-      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
-      delay = 700,
-      ignore_whitespace = false,
-    },
-    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
-    sign_priority = 6,
-    update_debounce = 100,
-    status_formatter = nil, -- Use default
-    max_file_length = 40000,
-    preview_config = {
-      -- Options passed to nvim_open_win
-      style = "minimal",
-      relative = "cursor",
-      row = 0,
-      col = 1,
-    },
 
     on_attach = function(bufnr)
       local gs = package.loaded.gitsigns
@@ -42,46 +14,33 @@ return {
         vim.keymap.set(mode, l, r, opts)
       end
 
-      -- Navigation (]h/[h for hunks)
-      map("n", "]h", function()
+      -- Navigation (]c/[c for changes - works in both diff and normal mode)
+      map("n", "]c", function()
         if vim.wo.diff then
-          return "]c"
-        end
-        vim.schedule(function()
+          vim.cmd.normal({ "]c", bang = true })
+        else
           gs.next_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true, desc = "Next hunk" })
-
-      map("n", "[h", function()
-        if vim.wo.diff then
-          return "[c"
         end
-        vim.schedule(function()
+      end, { desc = "Next change" })
+
+      map("n", "[c", function()
+        if vim.wo.diff then
+          vim.cmd.normal({ "[c", bang = true })
+        else
           gs.prev_hunk()
-        end)
-        return "<Ignore>"
-      end, { expr = true, desc = "Previous hunk" })
+        end
+      end, { desc = "Prev change" })
 
       -- Actions
-      map({ "n", "v" }, "<leader>ghs", function()
+      map("n", "<leader>ghs", gs.stage_hunk, { desc = "Stage hunk" })
+      map("n", "<leader>ghr", gs.reset_hunk, { desc = "Reset hunk" })
+      map("v", "<leader>ghs", function()
         gs.stage_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end)
-      map({ "n", "v" }, "<leader>ghr", function()
+      end, { desc = "Stage hunk" })
+      map("v", "<leader>ghr", function()
         gs.reset_hunk({ vim.fn.line("."), vim.fn.line("v") })
-      end)
-      map("n", "<leader>ghS", gs.stage_buffer)
-      map("n", "<leader>ghu", gs.undo_stage_hunk)
-      map("n", "<leader>ghR", gs.reset_buffer)
-      map("n", "<leader>ghp", gs.preview_hunk)
-      map("n", "<leader>ghd", function()
-        gs.diffthis("~")
-      end)
-      map("n", "<leader>ght", gs.toggle_deleted)
-      map("n", "<leader>ghb", gs.toggle_current_line_blame)
-
-      -- Text object
-      map({ "o", "x" }, "ih", ":<C-U>Gitsigns select_hunk<CR>")
+      end, { desc = "Reset hunk" })
+      map("n", "<leader>ghb", gs.toggle_current_line_blame, { desc = "Toggle blame" })
     end,
   },
 }
