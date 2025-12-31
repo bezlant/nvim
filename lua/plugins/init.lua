d0b32f6483c40dadc31036a3865edbf8ca9efd96
bezlant <abezlyudniy@Snowcrash.local>
2025-12-31 13:09:51 +0800

    fix: oil and tmux conflct

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -28,7 +28,17 @@ return {
     keys = {
       { "-", "<CMD>Oil<CR>", desc = "Oil File Browser" },
     },
-    config = true,
+    opts = {
+      keymaps = {
+        -- Disable defaults that conflict with tmux navigator
+        ["<C-h>"] = false,
+        ["<C-l>"] = false,
+        -- Keep useful oil keymaps
+        ["<C-r>"] = "actions.refresh",
+        ["<C-s>"] = { "actions.select", opts = { vertical = true } },
+        ["<C-v>"] = { "actions.select", opts = { horizontal = true } },
+      },
+    },
     dependencies = { "nvim-tree/nvim-web-devicons" },
   },
 
