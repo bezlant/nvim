edf211bc2d19239249050c830aa2493edbe840c6
bezlant <abezlyudniy@Snowcrash.local>
2025-12-31 17:55:18 +0800

    fix(treesitter): removed jump to context

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -172,15 +172,6 @@ return {
     opts = {
       max_lines = 3,
     },
-    keys = {
-      {
-        "[c",
-        function()
-          require("treesitter-context").go_to_context(vim.v.count1)
-        end,
-        desc = "Go to context",
-      },
-    },
   },
 
   -- Auto close/rename HTML tags
